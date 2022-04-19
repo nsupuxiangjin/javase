6f11a8d91a20663a4842de560a58de18e03631f1
puxiangjin <493252882@qq.com>
2022-04-19 17:28:58 +0800

    1111

--- oop-app/src/com/pxj/thisDemo/Car.java
+++ oop-app/src/com/pxj/thisDemo/Car.java
@@ -1,8 +1,21 @@
 package com.pxj.thisDemo;
 
 public class Car {
+    String name;
+    double price;
+
+    // 有参构造
+    public Car(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public Car() {
         System.out.println("无参构造中的this:" + this);
+    }
+
+    public void goWith(String name) {
+        System.out.println(this.name + "和正在和" + name + "比赛");
     }
 
     public void run() {
--- oop-app/src/com/pxj/thisDemo/Test.java
+++ oop-app/src/com/pxj/thisDemo/Test.java
@@ -5,5 +5,12 @@ public class Test {
         Car c = new Car();
         c.run(); // 无参构造中的this:com.pxj.thisDemo.Car@1b6d3586
         System.out.println(c); // com.pxj.thisDemo.Car@1b6d3586
+
+        System.out.println("-------------");
+        Car c2 = new Car("奔驰", 45.6);
+        System.out.println(c2.name);
+        System.out.println(c2.price);
+
+        c2.goWith("宝马");
     }
 }
